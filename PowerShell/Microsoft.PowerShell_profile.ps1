d8ce9680238b6eeca5b5a0ae3ea4e9c888df7eba
AmxnMehra <amanmehra0112233@gmail.com>
2025-10-07 23:50:53 +0530

    add PowerShell profile and oh-my-posh theme for terminal customization

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,30 @@
+# Minimal profile: UTF‑8 + Oh My Posh (if installed) + Fastfetch with explicit config path
+try {
+    [Console]::InputEncoding  = [System.Text.Encoding]::UTF8
+    [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+    $OutputEncoding = [System.Text.UTF8Encoding]::new($false)
+    chcp 65001 > $null
+} catch {}
+
+Clear-Host
+
+# Force Fastfetch to use YOUR config every time (bypass path confusion)
+if (Get-Command fastfetch -ErrorAction SilentlyContinue) {
+    fastfetch -c "C:/Users/amanm/.config/fastfetch/config.jsonc"
+}
+
+oh-my-posh init pwsh --config 'C:\Users\amanm\AppData\Local\Programs\oh-my-posh\themes\1_shell.omp.json' | Invoke-Expression
+
+
+# terminal icons
+Import-Module -Name Terminal-Icons
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# aliases
+Set-Alias -Name c -Value clear
